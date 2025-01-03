2e821c33dec7b15b0f95ec6a8f4676e97d79328d
Khairul <ahmadatun@gmail.com>
2025-01-04 01:56:06 +0800

    register updated

--- app/src/main/java/com/example/brainpath/ui/profile/RegisterActivity.java
+++ app/src/main/java/com/example/brainpath/ui/profile/RegisterActivity.java
@@ -41,11 +41,6 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up); // Ensure this matches your XML layout file name
 
-        // Hide the Action Bar
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().hide();
-        }
-
         // Initialize Firebase Authentication
         mAuth = FirebaseAuth.getInstance();
 
